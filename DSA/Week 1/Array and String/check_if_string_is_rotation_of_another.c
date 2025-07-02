070764bf04dee05044c64a7d8b38159caad8d0ba
JavaPyWizard <divyanshkumarsingh29@gmail.com>
2025-07-02 17:11:06 +0530

    Done Further Questions in Templated added yesterday

--- DSA/Week 1/Array and String/check_if_string_is_rotation_of_another.c
+++ DSA/Week 1/Array and String/check_if_string_is_rotation_of_another.c
@@ -2,8 +2,48 @@
 // Problem implementation in C
 
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
-int main() {
-    // Your code here
+bool isRotation(char str[], char substr[])
+{
+    int n = strlen(str);
+    int m = strlen(substr);
+
+    if (m > n)
+        return false;
+
+    for (int i = 0; i <= n - m; i++)
+    {
+        int j = 0;
+        while (j < m && str[i + j] == substr[j])
+            j++;
+
+        if (j == m)
+            return j;
+    }
+
+    return -1;
+}
+
+int main()
+{
+    char str[100], substr[100];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0';
+
+    printf("Enter substring: ");
+    fgets(substr, sizeof(substr), stdin);
+    substr[strcspn(substr, "\n")] = '\0';
+    char temp[100];
+    strcpy(temp,str);
+    strcat(temp,str);
+    if (isRotation(temp, substr))
+        printf("\"%s\" is a rotation of \"%s\".\n", substr, str);
+    else
+        printf("\"%s\" is not a rotation of \"%s\".\n", substr, str);
+
     return 0;
 }
