4792941f4c00f949b609e3629bc072df9a0bb5dd
JavaPyWizard <divyanshkumarsingh29@gmail.com>
2025-07-02 15:31:00 +0530

    Done Further Questions in Templated added yesterday

--- DSA/Week 1/Array and String/check_if_string_is_a_palindrome.c
+++ DSA/Week 1/Array and String/check_if_string_is_a_palindrome.c
@@ -2,8 +2,35 @@
 // Problem implementation in C
 
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
-int main() {
-    // Your code here
+bool isPalindrome(char str[100])
+{
+    int len = strlen(str);
+    char rev[100];
+
+    for (int i = len - 1, j = 0; i >= 0; i--, j++)
+    {
+        rev[j] = str[i];
+    }
+    rev[len] = '\0'; 
+
+    return strcmp(str, rev) == 0;
+}
+
+int main()
+{
+    char str[100];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0';
+
+    if (isPalindrome(str))
+        printf("\"%s\" is a Palindrome String.\n", str);
+    else
+        printf("\"%s\" is not a Palindrome String.\n", str);
+
     return 0;
 }
