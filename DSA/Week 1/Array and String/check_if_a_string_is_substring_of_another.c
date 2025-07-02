9b3ab3b9b08f6fb7094e8c2db36e9f3146689c14
JavaPyWizard <divyanshkumarsingh29@gmail.com>
2025-07-02 15:18:36 +0530

    Done Further Questions in Templated added yesterday

--- DSA/Week 1/Array and String/check_if_a_string_is_substring_of_another.c
+++ DSA/Week 1/Array and String/check_if_a_string_is_substring_of_another.c
@@ -2,8 +2,45 @@
 // Problem implementation in C
 
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
-int main() {
-    // Your code here
+// Function to check if substr is a substring of str
+bool isSub(char str[], char substr[]) {
+    int n = strlen(str);
+    int m = strlen(substr);
+
+    if (m > n) return false;
+
+    for (int i = 0; i <= n - m; i++) 
+    {
+        int j = 0;
+        while (j < m && str[i + j] == substr[j])
+            j++;
+
+        if (j == m) 
+            return j;
+    }
+
+    return -1;
+}
+
+int main() 
+{
+    char str[100], substr[100];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0';  
+
+    printf("Enter substring: ");
+    fgets(substr, sizeof(substr), stdin);
+    substr[strcspn(substr, "\n")] = '\0';  
+
+    if (isSub(str, substr))
+        printf("\"%s\" is a substring of \"%s\".\n", substr, str);
+    else
+        printf("\"%s\" is not a substring of \"%s\".\n", substr, str);
+
     return 0;
 }
--- DSA/Week 1/Array and String/check_if_array_is_sorted_asc_desc.c
+++ DSA/Week 1/Array and String/check_if_array_is_sorted_asc_desc.c
@@ -3,7 +3,32 @@
 
 #include <stdio.h>
 
-int main() {
-    // Your code here
+int main()
+{
+    int arr[100], n;
+    int isAsc = 1, isDesc = 1;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (arr[i] < arr[i + 1])
+            isDesc = 0;
+        else if (arr[i] > arr[i + 1])
+            isAsc = 0;
+    }
+
+    if (isAsc)
+        printf("The array is sorted in ascending order.\n");
+    else if (isDesc)
+        printf("The array is sorted in descending order.\n");
+    else
+        printf("The array is not sorted.\n");
+
     return 0;
 }
