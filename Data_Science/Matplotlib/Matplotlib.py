5118d205c1b49a900935f6ed48f147dcee238d13
JavaPyWizard <divyanshkumarsingh29@gmail.com>
2025-07-21 13:27:17 +0530

    Week 3

---
+++ Data_Science/Matplotlib/Matplotlib.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+
+# kohli = [0, 0, 500, 800, 1100, 1300, 1500, 1800, 1900, 2100]
+# sehwag = [0, 300, 800, 1200, 1500, 1700, 1600, 1400, 1000, 0]
+# years = [1990, 1992, 1994, 1996, 1998, 2000, 2003, 2005, 2007, 2010]
+
+# with plt.xkcd():
+#     plt.plot(years, kohli, color='orange', linestyle='--', linewidth=3, label="Kohli")
+#     plt.plot(years, sehwag, color='green', linestyle='-.', label="Sehwag")
+#     plt.title("Runs Scored")
+#     plt.xlabel("Year")
+#     plt.ylabel("Runs Scored")
+#     plt.grid(True, linestyle=':', linewidth=1, color='gray')  # <-- Force grid styling
+#     plt.tight_layout()
+#     plt.legend()
+#     plt.show()
+
+# plt.xlabel("X axis")
+# plt.ylabel("Y axis")
+# plt.plot(years, kohli, color='orange', linestyle='--', linewidth=3, label="Kohli")
+# plt.plot(years, sehwag, color='green', linestyle='-.', label="Sehwag")
+# plt.grid(True)
+# plt.tight_layout()
+# plt.legend()
+# plt.show()
+
+# # plt.plot([1, 2, 3], [4, 5, 6])
+# years = [1990, 1992, 1994, 1996, 1998, 2000, 2003, 2005, 2007, 2010]
+# runs =  [500, 700, 1100, 1500, 1800, 1200, 1700, 1300, 900, 1500]
+
+# plt.plot(years, runs)
+# plt.show()
