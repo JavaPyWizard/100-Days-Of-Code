e36c545be40af3af2d2a5523b49ce730e1544d18
JavaPyWizard <divyanshkumarsingh29@gmail.com>
2025-07-21 13:29:21 +0530

    Week 3

--- Data_Science/Matplotlib/Matplotlib.py
+++ Data_Science/Matplotlib/Matplotlib.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 # kohli = [0, 0, 500, 800, 1100, 1300, 1500, 1800, 1900, 2100]
 # sehwag = [0, 300, 800, 1200, 1500, 1700, 1600, 1400, 1000, 0]
